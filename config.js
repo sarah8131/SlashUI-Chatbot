4242a4d1a517dcd435d102f034ec9121d1295be3
Sarah Ghanbarzadeh <sarah_8131@yahoo.com>
2017-08-19 23:03:54 +0800

    config.js has been updated

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAABl3slnjIEBAEf0PLnZCFP1ONNZBy93XCGdoVnF218oPEV3MDZBd7L8QrptI1yK7ZAdap1oZAoc8GPnzlZAZAIXcaZB45wpoSTyrB9b7jG0A9pI8E1pkYFUTGytTrUaUZAaSit4w9dc0nmjJ1aZC5Jj0n69ja6x8NsxwsA8nx5S2evesxGfKfVBsQ',
-    FB_VERIFY_TOKEN: 'copper explain notice truck neat',
-    API_AI_CLIENT_ACCESS_TOKEN: '1bce24d160a44cd8b09d6e3903065ace',
-    FB_APP_SECRET: '908697120bd86acbbc067e5a016581c0',
-    SERVER_URL: 'https://slashui.herokuapp.com/webhook/',
+    FB_PAGE_TOKEN: 'EAABkhsdaggQBAP0jgHAGNYaBJFnn93kP2B4vqmiTdEeYLEchNbn0oM3DOeJpDn0DvzNXavTtJuZAM7WDhIxkBuFvf0hDY1DCSric0Oyqi7sAf9q9wjjSrKFbdKMYTuGwLSalZAEEu1ZC9krINceneSgNKY6EeCVz0qXaBv8DQZDZD',
+    FB_VERIFY_TOKEN: 'Fade2black',
+    API_AI_CLIENT_ACCESS_TOKEN: '08a59b32831c4562af016df5f11b7ca4',
+    FB_APP_SECRET: 'f3ded015e7e8b9b0c148c5f4954981ac',
+    SERVER_URL: 'https://thawing-stream-95955.herokuapp.com/webhook/',
 };
